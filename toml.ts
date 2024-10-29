d72ae32133cdf0dabed2a62a10430d51e3cf38ad
mactkg <mactkg@gmail.com>
2024-10-29 09:42:56 +0900

    Debug log in toml.ts

--- toml.ts
+++ toml.ts
@@ -1,5 +1,7 @@
 import xdg from "https://deno.land/x/xdg_portable@v10.6.0/src/mod.deno.ts";
 import { parse as parseToml } from "jsr:@std/toml";
+import { logger } from "./logger.ts";
+import { load } from "@std/dotenv";
 
 export interface TomlConfig {
   states: Record<string, Status>;
@@ -23,9 +25,12 @@ export async function loadConfig(fileName: string): Promise<TomlConfig | null> {
       await Deno.lstat(path);
       const toml = await Deno.readTextFile(path);
       const config = parseToml(toml);
+      logger.debug("toml config loaded: ");
+      logger.debug(JSON.stringify(config, null, 2));
+
       return config as unknown as TomlConfig;
-    } catch (_e) {
-      continue;
+    } catch (e) {
+      logger.error(e);
     }
   }
 
