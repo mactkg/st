bc43f8af148eec9676510aad450946710131e84f
mactkg <mactkg@gmail.com>
2024-10-29 09:43:07 +0900

    Implement generate command

--- toml.ts
+++ toml.ts
@@ -1,5 +1,5 @@
 import xdg from "https://deno.land/x/xdg_portable@v10.6.0/src/mod.deno.ts";
-import { parse as parseToml } from "jsr:@std/toml";
+import { parse as parseToml, stringify } from "jsr:@std/toml";
 import { logger } from "./logger.ts";
 import { load } from "@std/dotenv";
 
@@ -35,4 +35,33 @@ export async function loadConfig(fileName: string): Promise<TomlConfig | null> {
   }
 
   return null;
+}
+
+export async function generateState(
+  fileName: string,
+  name: string,
+  text: string,
+  emoji: string,
+) {
+  const newState = {
+    states: {
+      [name]: {
+        text,
+        emoji,
+        messages: [],
+      },
+    },
+  };
+  const tomlStr = stringify(newState);
+  const dir = xdg.config();
+  const path = `${dir}/st/${fileName}`;
+  try {
+    await Deno.writeTextFile(path, tomlStr, {
+      append: true,
+      create: true,
+    });
+    logger.info(`New state "${name}" was added to ${path}.`);
+  } catch (e) {
+    logger.error(e);
+  }
 }
